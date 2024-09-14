a761614f8bcbc454e2a2a284c2b6d6d81b870e19
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-09-14 12:16:35 +1000

    feat: converted cards to class and implmeneted lifecycle method

--- reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
+++ reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
@@ -10,42 +10,67 @@ export default class PokemonFetcher extends React.Component {
 		super(props);
 
 		this.state = {
-			pokemonList: []
+			pokemonList: [],
+			// pokemon1: null,
+			// pokemon2: null
+			userJwt: null
 		}
 	}
 
-    // runs automatically ONCE when the page first loads
-    async componentDidMount(){
-        // generate a random number for random Pokemon number
-        let randomNumber = Math.floor(Math.random() * 151) +1;
+	// runs automatically ONCE when the page first loads 
+	async componentDidMount(){
 
-        //pass random Pokemon number into fetch request
-		let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
-		let data = await response.json();
+		for (let index = 0; index < 6; index++) {
+			
+			// generate a random number for random Pokemon number 
+			let randomNumber = Math.floor(Math.random() * 151) + 1;
 
-        // set fetch response data into state
-        this.setState({pokemonList: [data]});
+			// pass random Pokemon number into fetch request
+			let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
+			let data = await response.json();
+
+			// set fetch response data into state 
+			// this.state.pokemonList.push(data);
+			// this.setState({pokemonList: [data]});
+			// this.setState({pokemonList: [...this.state.pokemonList, data]});
+			// this.setState({pokemonList: [...this.state.pokemonList, data]});
+			this.setState((previousState) => {
+				return {
+					pokemonList: [...previousState.pokemonList, data]
+				}
+			});
+			
+		}
+		
 
 		console.log("PokemonFetcher first load on the page");
-        
-    }
+	}
+
+	componentDidUpdate(){
+		console.log("Page has rendered! The current state is: " + this.state.pokemonList);
+
+		if (this.state.userJwt){
+			console.log("User is logged in!");
+		} else {
+			console.log("User is logged out");
+		}
+	}
 
 	render(){
 		return (
 			<div>
 				<h1>Pokemon Data</h1>
-				{this.state.pokemonList.map(pokemon => {
-					return <PokemonCard name={pokemon.name} />
+				{this.state.pokemonList.map((pokemon, index) => {
+					return <PokemonCard key={pokemon.name + index} name={pokemon.name} />
 				})}
 
-                <button onClick={() => {
-                    this.setState({pokemonList: []});
-                }}>
-                    Empty the state
-                    </button>
+				<button onClick={() => {
+					this.setState({pokemonList: []});
+				}}>
+					Empty the state
+				</button>
 			</div>
 			
 		);
 	}
 }
-
